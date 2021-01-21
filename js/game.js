5cdf9e112ab8d3fc823ad7cf094fb8eb1c73588b
AlvaroJTorres <mangetrone@gmail.com>
2021-01-21 13:38:14 -0500

    Create a function to generate an array with the colors to be assing to each square

--- js/game.js
+++ js/game.js
@@ -1,3 +1,15 @@
+let start_squares = 6;
+
+function listOfColors(squares) {
+    let color_list = []
+
+    for(let i = 0; i < squares; i++) {
+        color_list.push(randomColor())
+    }
+
+    return color_list
+}
+
 function randomColor() {
     let r = Math.floor(Math.random() * 256)
         g = Math.floor(Math.random() * 256)
