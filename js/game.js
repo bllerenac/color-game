a2460d401182c4244cd6af9f19452e85ed9efed0
AlvaroJTorres <mangetrone@gmail.com>
2021-01-21 13:29:45 -0500

    Create the function randomColor to genereate a random rgbvalue for a square

---
+++ js/game.js
@@ -0,0 +1,7 @@
+function randomColor() {
+    let r = Math.floor(Math.random() * 256)
+        g = Math.floor(Math.random() * 256)
+        b = Math.floor(Math.random() * 256)
+    
+        return "rgb(" + r + ", " + g + ", " + b + ")"
+}
